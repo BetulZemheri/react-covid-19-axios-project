3140af9fbc75f279fac13374296a6b0dc174ee8a
Betul Zemheri <a.betul2200@gmail.com>
2022-07-18 18:38:54 +0300

    covid-19 axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+    .then(res => setData(res.data[date]))
+    .catch(err => console.log(err))
+  }, [data,date])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h4 className="text-center text-white display-3">TURKEY COVID-19 SEARCH ENGINE</h4>
+            <input type="text" placeholder="GG/AA/YY" className="form-control" onChange={(e) => setDate(e.target.value)}/>
+            <table className="table table-striped text-white">
+              <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Number Of Tests</th>
+                <th scope="col">Number Of Patients</th>
+                <th scope="col">Number Of Deaths</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className={data === undefined ? "bg-danger" : "bg-success"}>
+                <th scope="row"> {data === undefined ? "waiting for data" : data.date} </th>
+                <td className="text-white" > {data === undefined ? "waiting for data" : data.totalTests} </td>
+                <td className="text-white" > {data === undefined ? "waiting for data" : data.patients} </td>
+                <td className="text-white" > {data === undefined ? "waiting for data" : data.deaths} </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
